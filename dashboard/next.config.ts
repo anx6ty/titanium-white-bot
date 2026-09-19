1d6616d524f8019a707d5f3cf162f7032284b60b
anx6ty <anx6ty@gmail.com>
2026-09-19 08:11:53 +0500

    Delete dashboard directory

--- dashboard/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  poweredByHeader: false,
-  reactStrictMode: true,
-  output: "standalone"
-};
-
-export default nextConfig;
